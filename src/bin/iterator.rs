53470199d07f85ed3372a7b4990c1636fb89736a
amaretto <jabberwock26@gmail.com>
2022-05-07 13:51:15 +0900

    finish implement iterator

--- src/bin/iterator.rs
+++ src/bin/iterator.rs
@@ -1,33 +1,71 @@
 pub trait Iterator {
     type Item;
-    fn hasNext(&mut self) -> bool;
+    fn has_next(&self) -> bool;
     fn next(&mut self) -> &Self::Item;
 }
 
 struct Music {
-    id: u64,
-    title: &'static str,
-    artist: &'static str,
+    title: String,
 }
 
 struct Album {
-    curr: usize,
-    next: usize,
-    musics: Vec<&'static Music>,
+    title: String,
+    musics: Vec<Music>,
 }
 
-impl Iterator for Album {
+struct AlbumIterator<'a> {
+    album: &'a Album,
+    index: usize,
+}
+
+impl Album {
+    fn iter(&self) -> AlbumIterator {
+        AlbumIterator {
+            album: self,
+            index: 0,
+        }
+    }
+}
+
+impl Iterator for AlbumIterator<'_> {
     type Item = Music;
-    fn hasNext(&mut self) -> bool {
-        true
+    fn has_next(&self) -> bool {
+        if self.index < self.album.musics.len() {
+            return true;
+        }
+        false
     }
 
     fn next(&mut self) -> &Self::Item {
-        let result = self.musics[self.curr];
-        &result
+        let result = &self.album.musics[self.index];
+        self.index += 1;
+        return result;
     }
 }
 
 fn main() {
-    println!("hoge")
+    let song_a = Music {
+        title: String::from("hoge"),
+    };
+    let song_b = Music {
+        title: String::from("fuga"),
+    };
+    let song_c = Music {
+        title: String::from("foo"),
+    };
+    let song_d = Music {
+        title: String::from("bar"),
+    };
+
+    let album = Album {
+        title: String::from("hogera"),
+        musics: vec![song_a, song_b, song_c, song_d],
+    };
+    println!("Album:{}", album.title);
+
+    let mut it = album.iter();
+    while it.has_next() {
+        let music = it.next();
+        println!("  {}", music.title)
+    }
 }
